f1719b5d7891826a6161b7991c80e271c33b6089
Dyarlen Iber <dyarlen1@gmail.com>
2020-11-28 17:57:53 -0300

    Add Tailwind CSS Purge again

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,8 +3,8 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Your Name...";
-export const siteTitle = "Next.js Sample Website";
+const name = "Your Name.";
+export const siteTitle = "Next.js Sample Website.";
 
 export default function Layout({ children, home }) {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  //purge: ["./src/**/*.{js,ts,jsx,tsx}"],
-  purge: [],
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
