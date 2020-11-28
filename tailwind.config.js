05483cf28bf351cb5543854d21d21f807b342dbc
Dyarlen Iber <dyarlen1@gmail.com>
2020-11-28 17:48:31 -0300

    Remove purge for testing purpose

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  //purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
