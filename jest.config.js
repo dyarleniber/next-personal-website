b5e8af79670cc0f6db210ea69c60cbc511fe2ed3
Dyarlen Iber <dyarlen1@gmail.com>
2020-12-18 11:14:14 -0300

    Add mock for CSS Modules

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  setupFilesAfterEnv: ["<rootDir>/src/__tests__/testSetupFile.js"],
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setupTests.js"],
   testMatch: ["**/__tests__/**/*.test.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  transform: {
+    "\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
 };
